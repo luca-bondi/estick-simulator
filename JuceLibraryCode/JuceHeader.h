5f7b182c6af321d19fccf24144a9b4f8ebbfa13c
Luca Bondi <luca.bondi@polimi.it>
2020-05-05 14:53:43 +0200

    Version with decimals

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -50,7 +50,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "eStick Simulator";
     const char* const  companyName    = "Luca Bondi";
-    const char* const  versionString  = "1";
+    const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
 #endif
